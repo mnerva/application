cf28c070b440e4d6a6cdedb072b2d74a7455f1cd
mona <mona-rea.nerva@voco.ee>
2024-10-01 12:44:21 +0300

    added Dashboard button into sidebar

--- public/calendar.js
+++ public/calendar.js
@@ -8,10 +8,18 @@ function calendar() {
     window.location.href = 'calendar.html';
 }
 
+function dashboard() {
+    window.location.href = 'dashboard.html';
+}
+
 if (document.getElementById('logoutBtn')) {
     document.getElementById('logoutBtn').addEventListener('click', logout);
 }
 
 if (document.getElementById('calendarBtn')) {
     document.getElementById('calendarBtn').addEventListener('click', calendar);
+}
+
+if (document.getElementById('dashBtn')) {
+    document.getElementById('dashBtn').addEventListener('click', dashboard);
 }
