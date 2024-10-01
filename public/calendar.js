a62922df0724b94bfcbad7d428194b98f63699a0
mona <mona-rea.nerva@voco.ee>
2024-10-01 12:38:35 +0300

    added sidebar

--- public/calendar.js
+++ public/calendar.js
@@ -4,6 +4,14 @@ function logout() {
     window.location.href = 'index.html';
 }
 
+function calendar() {
+    window.location.href = 'calendar.html';
+}
+
 if (document.getElementById('logoutBtn')) {
     document.getElementById('logoutBtn').addEventListener('click', logout);
+}
+
+if (document.getElementById('calendarBtn')) {
+    document.getElementById('calendarBtn').addEventListener('click', calendar);
 }
